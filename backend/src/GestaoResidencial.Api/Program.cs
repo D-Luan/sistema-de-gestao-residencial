df4e5b06d29ba692f9caf298bb00ad0eb4c72fc3
D-luan <dluan1285@gmail.com>
2026-03-20 22:00:28 -0300

    feat(db): adiciona docker compose e dbcontext

--- backend/src/GestaoResidencial.Api/Program.cs
+++ backend/src/GestaoResidencial.Api/Program.cs
@@ -1,6 +1,12 @@
+using GestaoResidencial.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
