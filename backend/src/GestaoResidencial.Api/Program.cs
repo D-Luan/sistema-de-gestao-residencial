2a0d2e44fd11043955960ea8b56fc5b0d144ff61
D-luan <dluan1285@gmail.com>
2026-03-20 22:17:00 -0300

    chore(api): configura o CORS e a documentação da API via Scalar

--- backend/src/GestaoResidencial.Api/Program.cs
+++ backend/src/GestaoResidencial.Api/Program.cs
@@ -1,5 +1,6 @@
 using GestaoResidencial.Api.Data;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,13 +9,28 @@ builder.Services.AddOpenApi();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.Run();
